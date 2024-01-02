7cce7965a4df9bb7126e4d6307fb01a78d31edca
= <=>
2024-01-02 16:50:29 +0100

    Event handlers

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -33,6 +33,22 @@ const hasError = ref(true)
 const activeClass = ref('active')
 const errorClass = ref('text-danger')
 
+//conditional rendering
+const awesome = ref(true)
+const show = ref(true)
+
+const toggleVue = () => awesome.value = !awesome.value
+
+
+//loopx
+const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+const myObject = reactive({
+  title: 'How to do lists in Vue',
+  author: 'Jane Doe',
+  publishedAt: '2016-04-10'
+})
+
+alert("hello world")
 </script>
 
 <template>
@@ -44,7 +60,29 @@ const errorClass = ref('text-danger')
     <button @click="count--">Decrement Count</button>
     <p class="styles" :class="{acive: isActive, 'text-danger':hasError}">Classes and Styles</p>
     <div :class="[{ activeClass: isActive }, errorClass]"></div>
+     <div class="cond-rederx">
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh no 😢</h1>
+    <h1 v-show="show">Show me</h1>
+      <button @click="toggleVue">Toggle Vue with Method</button>
+      <button @click="awesome = !awesome">Toggle Vue</button>
+      <button @click="show = !show">Toggle Show</button>
+
   </div>
+     <div class="loopx">
+      <li v-for="({message}, index) of items" :key="index">
+     {{ index }} : {{ message }}
+      </li>
+      <li v-for="(value,key,i) in myObject" :key="i">
+      {{ i }} => {{ key }} : {{ value }}
+      </li>
+      <span v-for="n in 10" :key="n">
+        {{ n }}
+      </span>
+     </div>
+  </div>
+
+ 
 </template>
 
 <style>
